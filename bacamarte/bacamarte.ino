679977e8652bf48d1ef8ad7373b18573b7e9d979
Paulo Borges <pauloborgesfilho@gmail.com>
2012-06-26 20:17:26 -0300

    First commit

---
+++ bacamarte/bacamarte.ino
@@ -0,0 +1,19 @@
+int first_pad = A0;
+int last_pad = A5;
+int value;
+
+void setup()
+{
+  Serial.begin(57600);
+}
+
+void loop()
+{
+  for (int pad = first_pad; pad <= last_pad; pad++)
+  {
+    value = analogRead(pad);
+    Serial.print(pad-first_pad);
+    Serial.print(',');
+    Serial.println(value);
+  }
+}
